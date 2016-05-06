e8f4150a839ec901538b0659cbf543d1b606de63
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-05-06 20:14:47 +0300

    Update boot.ts

--- app/boot.ts
+++ app/boot.ts
@@ -3,7 +3,3 @@ import {UpgradeAdapter} from '@angular/upgrade';
 const adapter = new UpgradeAdapter();
 
 adapter.bootstrap(document.body, ['todoListWorkshopApp']);
-//import {bootstrap}    from '@angular/platform-browser-dynamic';
-//import {AppComponent} from './app.component';
-
-//bootstrap(AppComponent);
