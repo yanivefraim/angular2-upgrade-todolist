78965d86072e77db6d2143b5d41a4d75a8df58f5
Yaniv Efraim <yaniv.efraim@gmail.com>
2016-05-07 12:13:13 +0300

    remove boot and systemjs from upgrade starting point

--- app/boot.ts
+++
@@ -1,9 +0,0 @@
-import {UpgradeAdapter} from '@angular/upgrade';
-
-const adapter = new UpgradeAdapter();
-
-adapter.bootstrap(document.body, ['todoListWorkshopApp']);
-//import {bootstrap}    from '@angular/platform-browser-dynamic';
-//import {AppComponent} from './app.component';
-
-//bootstrap(AppComponent);
